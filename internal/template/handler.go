d4945fe1b7024222b3c2c1e1c636f934fb8d424b
Jerry <85411418@qq.com>
2020-12-05 16:56:25 +0800

    first commit

---
+++ internal/template/handler.go
@@ -0,0 +1,206 @@
+package template
+
+var (
+	HandlerFNC = `package handler
+
+import (
+	"context"
+
+	{{dehyphen .Alias}} "{{.Dir}}/proto/{{.Alias}}"
+)
+
+type {{title .Alias}} struct{}
+
+// Call is a single request handler called via client.Call or the generated client code
+func (e *{{title .Alias}}) Call(ctx context.Context, req *{{dehyphen .Alias}}.Request, rsp *{{dehyphen .Alias}}.Response) error {
+	rsp.Msg = "Hello " + req.Name
+	return nil
+}
+`
+
+	HandlerSRV = `package handler
+
+import (
+	"context"
+
+	log "github.com/micro/go-micro/v2/logger"
+
+	{{dehyphen .Alias}} "{{.Dir}}/proto/{{.Alias}}"
+)
+
+type {{title .Alias}} struct{}
+
+// Call is a single request handler called via client.Call or the generated client code
+func (e *{{title .Alias}}) Call(ctx context.Context, req *{{dehyphen .Alias}}.Request, rsp *{{dehyphen .Alias}}.Response) error {
+	log.Info("Received {{title .Alias}}.Call request")
+	rsp.Msg = "Hello " + req.Name
+	return nil
+}
+
+// Stream is a server side stream handler called via client.Stream or the generated client code
+func (e *{{title .Alias}}) Stream(ctx context.Context, req *{{dehyphen .Alias}}.StreamingRequest, stream {{dehyphen .Alias}}.{{title .Alias}}_StreamStream) error {
+	log.Infof("Received {{title .Alias}}.Stream request with count: %d", req.Count)
+
+	for i := 0; i < int(req.Count); i++ {
+		log.Infof("Responding: %d", i)
+		if err := stream.Send(&{{dehyphen .Alias}}.StreamingResponse{
+			Count: int64(i),
+		}); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// PingPong is a bidirectional stream handler called via client.Stream or the generated client code
+func (e *{{title .Alias}}) PingPong(ctx context.Context, stream {{dehyphen .Alias}}.{{title .Alias}}_PingPongStream) error {
+	for {
+		req, err := stream.Recv()
+		if err != nil {
+			return err
+		}
+		log.Infof("Got ping %v", req.Stroke)
+		if err := stream.Send(&{{dehyphen .Alias}}.Pong{Stroke: req.Stroke}); err != nil {
+			return err
+		}
+	}
+}
+`
+
+	SubscriberFNC = `package subscriber
+
+import (
+	"context"
+
+	log "github.com/micro/go-micro/v2/logger"
+
+	{{dehyphen .Alias}} "{{.Dir}}/proto/{{.Alias}}"
+)
+
+type {{title .Alias}} struct{}
+
+func (e *{{title .Alias}}) Handle(ctx context.Context, msg *{{dehyphen .Alias}}.Message) error {
+	log.Info("Handler Received message: ", msg.Say)
+	return nil
+}
+`
+
+	SubscriberSRV = `package subscriber
+
+import (
+	"context"
+	log "github.com/micro/go-micro/v2/logger"
+
+	{{dehyphen .Alias}} "{{.Dir}}/proto/{{.Alias}}"
+)
+
+type {{title .Alias}} struct{}
+
+func (e *{{title .Alias}}) Handle(ctx context.Context, msg *{{dehyphen .Alias}}.Message) error {
+	log.Info("Handler Received message: ", msg.Say)
+	return nil
+}
+
+func Handler(ctx context.Context, msg *{{dehyphen .Alias}}.Message) error {
+	log.Info("Function Received message: ", msg.Say)
+	return nil
+}
+`
+
+	HandlerAPI = `package handler
+
+import (
+	"context"
+	"encoding/json"
+	log "github.com/micro/go-micro/v2/logger"
+
+	"{{.Dir}}/client"
+	"github.com/micro/go-micro/v2/errors"
+	api "github.com/micro/go-micro/v2/api/proto"
+	{{dehyphen .Alias}} "path/to/service/proto/{{.Alias}}"
+)
+
+type {{title .Alias}} struct{}
+
+func extractValue(pair *api.Pair) string {
+	if pair == nil {
+		return ""
+	}
+	if len(pair.Values) == 0 {
+		return ""
+	}
+	return pair.Values[0]
+}
+
+// {{title .Alias}}.Call is called by the API as /{{.Alias}}/call with post body {"name": "foo"}
+func (e *{{title .Alias}}) Call(ctx context.Context, req *api.Request, rsp *api.Response) error {
+	log.Info("Received {{title .Alias}}.Call request")
+
+	// extract the client from the context
+	{{dehyphen .Alias}}Client, ok := client.{{title .Alias}}FromContext(ctx)
+	if !ok {
+		return errors.InternalServerError("{{.FQDN}}.{{dehyphen .Alias}}.call", "{{dehyphen .Alias}} client not found")
+	}
+
+	// make request
+	response, err := {{dehyphen .Alias}}Client.Call(ctx, &{{dehyphen .Alias}}.Request{
+		Name: extractValue(req.Post["name"]),
+	})
+	if err != nil {
+		return errors.InternalServerError("{{.FQDN}}.{{dehyphen .Alias}}.call", err.Error())
+	}
+
+	b, _ := json.Marshal(response)
+
+	rsp.StatusCode = 200
+	rsp.Body = string(b)
+
+	return nil
+}
+`
+
+	HandlerWEB = `package handler
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/micro/go-micro/v2/client"
+	{{dehyphen .Alias}} "path/to/service/proto/{{.Alias}}"
+)
+
+func {{title .Alias}}Call(w http.ResponseWriter, r *http.Request) {
+	// decode the incoming request as json
+	var request map[string]interface{}
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	// call the backend service
+	{{dehyphen .Alias}}Client := {{dehyphen .Alias}}.New{{title .Alias}}Service("{{.Namespace}}.service.{{dehyphen .Alias}}", client.DefaultClient)
+	rsp, err := {{dehyphen .Alias}}Client.Call(context.TODO(), &{{dehyphen .Alias}}.Request{
+		Name: request["name"].(string),
+	})
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	// we want to augment the response
+	response := map[string]interface{}{
+		"msg": rsp.Msg,
+		"ref": time.Now().UnixNano(),
+	}
+
+	// encode and write the response as json
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+}
+`
+)
---
+++ test/misc_test.go
@@ -0,0 +1,182 @@
+// +build integration
+
+package test
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	trySuite(t, testNew, retryCount)
+}
+
+func testNew(t *t) {
+	t.Parallel()
+
+	tcs := []struct {
+		svcName    string
+		sType      string
+		skipBuild  bool
+		skipProtoc bool
+	}{
+		{svcName: "foobarsvc", sType: "service"},
+		{svcName: "foobarfn", sType: "function"},
+		{svcName: "foobarweb", sType: "web", skipProtoc: true, skipBuild: true}, // web service has no proto generated
+		{svcName: "foobarapi", sType: "api", skipBuild: true},                   // api service actually fails build out of the box because it's supposed to point to a service proto
+		{svcName: "foo-bar", sType: "service"},
+		{svcName: "foo-barfn", sType: "function"},
+		{svcName: "foo-barweb", sType: "web", skipProtoc: true, skipBuild: true}, // web service has no proto generated
+		{svcName: "foo-barapi", sType: "api", skipBuild: true},                   // api service actually fails build out of the box because it's supposed to point to a service proto
+		{svcName: "foo-bar-baz", sType: "service"},
+	}
+
+	for _, tc := range tcs {
+		t.t.Run(tc.svcName, func(t *testing.T) {
+			defer func() {
+				exec.Command("rm", "-r", "./"+tc.svcName).CombinedOutput()
+			}()
+			outp, err := exec.Command("micro", "new", "--type", tc.sType, tc.svcName).CombinedOutput()
+			if err != nil {
+				t.Fatal(err)
+				return
+			}
+			if !tc.skipProtoc && !strings.Contains(string(outp), "protoc") {
+				t.Fatalf("micro new lacks protobuf install instructions %v", string(outp))
+				return
+			}
+
+			lines := strings.Split(string(outp), "\n")
+			// executing install instructions
+			for _, line := range lines {
+				if strings.HasPrefix(line, "go get") {
+					parts := strings.Split(line, " ")
+					getOutp, getErr := exec.Command(parts[0], parts[1:]...).CombinedOutput()
+					if getErr != nil {
+						t.Fatal(string(getOutp))
+						return
+					}
+				}
+				if !tc.skipProtoc && strings.HasPrefix(line, "make proto") {
+					mp := strings.Split(line, " ")
+					protocCmd := exec.Command(mp[0], mp[1:]...)
+					protocCmd.Dir = "./" + tc.svcName
+					pOutp, pErr := protocCmd.CombinedOutput()
+					if pErr != nil {
+						t.Log("That didn't work ", pErr)
+						t.Fatal(string(pOutp))
+						return
+					}
+				}
+			}
+			if tc.skipBuild {
+				return
+			}
+			buildCommand := exec.Command("go", "build")
+			buildCommand.Dir = "./" + tc.svcName
+			outp, err = buildCommand.CombinedOutput()
+			if err != nil {
+				t.Fatal(string(outp))
+				return
+			}
+
+		})
+	}
+
+}
+
+func TestWrongCommands(t *testing.T) {
+	trySuite(t, testWrongCommands, retryCount)
+}
+
+func testWrongCommands(t *t) {
+	// @TODO this is obviously bad that we have to start a server for this. Why?
+	// What happens is in `cmd/cmd.go` `/service/store/cli/util.go`.SetupCommand is called
+	// which does not run for builtin services and help etc but there is no such exception for
+	// missing/unrecognized commands, so the behaviour below will only happen if a `micro server`
+	// is running. This is most likely because some config/auth wrapper in the background failing.
+	// Fix this later.
+	serv := newServer(t)
+	serv.launch()
+	defer serv.close()
+
+	t.Parallel()
+
+	comm := exec.Command("micro", serv.envFlag())
+	outp, err := comm.CombinedOutput()
+	if err == nil {
+		t.Fatal("Missing command should error")
+	}
+
+	if !strings.Contains(string(outp), "No command") {
+		t.Fatalf("Unexpected output for no command: %v", string(outp))
+	}
+
+	comm = exec.Command("micro", serv.envFlag(), "asdasd")
+	outp, err = comm.CombinedOutput()
+	if err == nil {
+		t.Fatal("Wrong command should error")
+	}
+
+	if !strings.Contains(string(outp), "Unrecognized micro command") {
+		t.Fatalf("Unexpected output for unrecognized command: %v", string(outp))
+	}
+
+	comm = exec.Command("micro", serv.envFlag(), "config", "asdasd")
+	outp, err = comm.CombinedOutput()
+	if err == nil {
+		t.Fatal("Wrong subcommand should error")
+	}
+
+	// @todod for some reason this one returns multiple lines so we don't check for line count now
+	if !strings.Contains(string(outp), "Unrecognized subcommand for micro config") {
+		t.Fatalf("Unexpected output for unrecognized subcommand: %v", string(outp))
+	}
+}
+
+// TestHelps ensures all `micro [command name] help` && `micro [command name] --help` commands are working.
+func TestHelps(t *testing.T) {
+	trySuite(t, testHelps, retryCount)
+}
+
+func testHelps(t *t) {
+	comm := exec.Command("micro", "help")
+	outp, err := comm.CombinedOutput()
+	if err != nil {
+		t.Fatal(err)
+	}
+	commands := strings.Split(strings.Split(string(outp), "COMMANDS:")[1], "GLOBAL OPTIONS:")[0]
+	for _, line := range strings.Split(commands, "\n") {
+		trimmed := strings.TrimSpace(line)
+		if len(trimmed) == 0 {
+			continue
+		}
+		// no help for help ;)
+		if strings.Contains(trimmed, "help") {
+			continue
+		}
+		commandName := strings.Split(trimmed, " ")[0]
+		comm = exec.Command("micro", commandName, "--help")
+		outp, err = comm.CombinedOutput()
+
+		if err != nil {
+			t.Fatal(fmt.Errorf("Command %v output is wrong: %v", commandName, string(outp)))
+			break
+		}
+		if !strings.Contains(string(outp), "micro "+commandName+" -") {
+			t.Fatal(commandName + " output is wrong: " + string(outp))
+			break
+		}
+	}
+}
+
+func TestUnrecognisedCommand(t *testing.T) {
+	t.Parallel()
+	outp, _ := exec.Command("micro", "foobar").CombinedOutput()
+	if !strings.Contains(string(outp), "Unrecognized micro command: foobar. Please refer to 'micro --help'") {
+		t.Fatalf("micro foobar does not return correct error %v", string(outp))
+		return
+	}
+}
